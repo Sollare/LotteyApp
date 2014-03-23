28128217c42da042bfcdec3ab8de6985b7984c40
Sollare <sollare@yandex.ru>
2014-03-23 19:13:33 +0400

    Json библиотека

---
+++ Assets/Scripts/ServerModels/ResponseMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace LotteyServerApp.Models
+{
+    [Serializable]
+    public class ResponseMessage
+    {
+        public int Code;
+        public string Message;
+
+        public ResponseMessage(int code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ Assets/Scripts/ServerModels/ViewModels/UserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+    public class Drawing
+    {
+        public int id;
+        public string name;
+        public int totalmoney;
+        public DateTime expiration;
+    }
+
+    public class User
+    {
+        public int id;
+        public string name;
+        public string email;
+
+        public Ticket[] freeTickets;
+        public Bet[] bets;
+        public Win[] victories;
+    }
+
+    public class Bet
+    {
+        public int id;
+        public int drawingId;
+    }
+    
+    public class FreeTicketViewModel
+    {
+        public int id;
+    }
+
+    public class Win
+    {
+        public int? drawingId;
+        public int totalMoney;
+    }
