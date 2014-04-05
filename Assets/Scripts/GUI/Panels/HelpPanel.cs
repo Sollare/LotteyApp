21e990ddd2733b3785a193c0b9c0f297a63b1245
Sollare <sollare@yandex.ru>
2014-04-05 12:44:48 +0400

    Окно помощи

---
+++ Assets/Scripts/GUI/Panels/HelpPanel.cs
@@ -0,0 +1,83 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class HelpPanel : MonoBehaviour
+{
+    private UIPanel panel;
+    private bool visible = false;
+
+    public UISprite ticketSilouette;
+    public TicketsGrid grid;
+    private Vector3 ticketInitialPosition;
+
+    public UISprite buyTicket;
+    public UILabel ticketLabel;
+
+    public UISprite ticketArrow;
+
+    void Awake()
+    {
+        ticketInitialPosition = ticketSilouette.cachedTransform.position;
+        panel = GetComponent<UIPanel>();
+    }
+
+    void OnPress(bool isPressed)
+    {
+        if (isPressed == false && UICamera.currentTouch.current == gameObject)
+            SetVisible(false);
+    }
+
+    public void Show()
+    {
+        SetVisible(true);
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetVisible(bool show)
+    {
+        if (show == visible) return;
+
+        ticketArrow.enabled = show;
+        visible = show;
+
+        var firstTicket = grid.FirstTicket;
+        
+        if (show)
+        {
+            if (firstTicket)
+            {
+                ticketLabel.text = "Drag the selected ticket in the lottery field";
+                ticketSilouette.cachedTransform.position = firstTicket.transform.position;
+            }
+            else
+            {
+                ticketLabel.text = "You have no tickets.\nBuy it!";
+                ticketSilouette.cachedTransform.position = buyTicket.cachedTransform.position;
+            }
+
+            gameObject.SetActive(true);
+            panel.alpha = 0f;
+
+            var tweener = TweenAlpha.Begin(gameObject, 0.3f, 1f);
+            tweener.onFinished.Clear();
+        }
+        else
+        {
+            
+
+            panel.alpha = 1f;
+
+            var tweener = TweenAlpha.Begin(gameObject, 0.3f, 0f);
+
+            tweener.onFinished.Clear();
+            tweener.AddOnFinished(new EventDelegate.Callback(delegate
+            {
+                gameObject.SetActive(false);
+            }));
+        }
+    }
+}
